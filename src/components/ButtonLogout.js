37150b0b99ed5d8548d4a68884fdf5f67556d99a
mazdel <delyachmad@gmail.com>
2024-01-28 11:39:40 +0700

    feat: add logout button

---
+++ src/components/ButtonLogout.js
@@ -0,0 +1,36 @@
+"use client";
+import PropTypes from "prop-types";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+
+const ButtonLogout = ({ children, className, onClick, ...moreProps }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove("access_token");
+    return router.refresh();
+  };
+
+  return (
+    <button
+      type="button"
+      className={className}
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        handleLogout();
+        if (onClick) return onClick(e);
+      }}
+      {...moreProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+ButtonLogout.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+};
+export { ButtonLogout };
