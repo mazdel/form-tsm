2918ea3a5cb8a1af5ce788069c1d5b6d89de461a
mazdel <delyachmad@gmail.com>
2024-01-28 11:34:25 +0700

    feat: add unprotected conf to exclude some path from security validation

--- src/configs/global.js
+++ src/configs/global.js
@@ -2,10 +2,11 @@ const globalConfig = {
   secret: process.env.GOOGLE_SHEETS_PRIVATE_KEY ?? "th1sIsAsecr3t",
   JWT: {
     alg: "HS512",
-    exp: "2h",
+    exp: "6h",
     secret: process.env.GOOGLE_SHEETS_PRIVATE_KEY ?? "th1sIsAsecr3t",
   },
-  protectedPath: ["/machines"],
+  protectedPath: ["/machines", "/api/v1"],
+  unProtectedPath: ["/api/v1/session"],
   defaultRedirectPath: "/",
 };
 
